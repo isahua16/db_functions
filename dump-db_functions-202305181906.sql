16c4f071d665917734aa4c87a7ca9c49e39b56f0
isahua16 <isaelhuard@gmail.com>
2023-05-18 19:07:44 -0600

    db backup and queries

---
+++ dump-db_functions-202305181906.sql
@@ -0,0 +1,94 @@
+-- MariaDB dump 10.19  Distrib 10.11.3-MariaDB, for osx10.16 (x86_64)
+--
+-- Host: localhost    Database: db_functions
+-- ------------------------------------------------------
+-- Server version	10.11.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cars_for_sale`
+--
+
+DROP TABLE IF EXISTS `cars_for_sale`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cars_for_sale` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dealership_id` int(10) unsigned DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `make` varchar(20) DEFAULT NULL,
+  `model` varchar(40) DEFAULT NULL,
+  `year` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cars_for_sale_FK` (`dealership_id`),
+  CONSTRAINT `cars_for_sale_FK` FOREIGN KEY (`dealership_id`) REFERENCES `dealership` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cars_for_sale`
+--
+
+LOCK TABLES `cars_for_sale` WRITE;
+/*!40000 ALTER TABLE `cars_for_sale` DISABLE KEYS */;
+INSERT INTO `cars_for_sale` VALUES
+(1,1,20000,'Toyota','RAV4',2018),
+(2,1,35000,'Ford','Escape',2022),
+(3,2,25000,'Mazda','3',2019),
+(4,2,45000,'Tesla','Model S',2023);
+/*!40000 ALTER TABLE `cars_for_sale` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dealership`
+--
+
+DROP TABLE IF EXISTS `dealership`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dealership` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `date_established` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dealership`
+--
+
+LOCK TABLES `dealership` WRITE;
+/*!40000 ALTER TABLE `dealership` DISABLE KEYS */;
+INSERT INTO `dealership` VALUES
+(1,'Dealership 1','Edmonton','2006-06-14'),
+(2,'Dealership 2','Red Deer','2012-08-10'),
+(3,'Dealership 3','Calgary','2019-03-30');
+/*!40000 ALTER TABLE `dealership` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_functions'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-18 19:06:34
---
+++ queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO dealership(name, location, date_established) VALUES('Dealership 1', 'Edmonton', '2006-06-14'), ('Dealership 2', 'Red Deer', '2012-08-10'), ('Dealership 3', 'Calgary', '2019-03-30');
+INSERT INTO cars_for_sale(dealership_id, price, make, model, `year`) VALUES(1, 20000.00, 'Toyota', 'RAV4', 2018), (1, 35000.00, 'Ford', 'Escape', 2022), (2, 25000.00, 'Mazda', '3', 2019), (2, 45000.00, 'Tesla', 'Model S', 2023);
+
+SELECT make, model, `year` from cars_for_sale ORDER BY `year` DESC;
+SELECT name, location from dealership d ORDER BY date_established ASC LIMIT 1;
+SELECT cfs.make, cfs.model, cfs.`year`, d.name, d.location from cars_for_sale cfs INNER JOIN dealership d ON cfs.dealership_id = d.id WHERE cfs.`year` > 2020 ORDER BY cfs.`year` ASC;
